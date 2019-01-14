d2c9000178b9aaa6cb983a5c3bc8535751a78e58
Streletz <streletzcoder@yandex.ru>
2019-01-14 19:55:33 +0300

    Добавлен задел для админпанели
     - Добавлены контроллер и представление для дашборда;
     - Добавлены контроллер и представление для категорий.

--- StreletzNoticeBoard/Startup.cs
+++ StreletzNoticeBoard/Startup.cs
@@ -66,6 +66,9 @@ namespace StreletzNoticeBoard
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                    name: "areas",
+                    template: "{area:exists}/{controller=Home}/{action=Index}");
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
