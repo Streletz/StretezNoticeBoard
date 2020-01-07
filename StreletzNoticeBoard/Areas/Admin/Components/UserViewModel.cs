e21a15e4cbb70ebd0a058ecd8171af0894126cd1
Streletz <streletzcoder@yandex.ru>
2020-01-08 00:00:41 +0300

    Пользователи
     - Вывод списка;
     - Заготовка шаблона карточки.

---
+++ StreletzNoticeBoard/Areas/Admin/Components/UserViewModel.cs
@@ -0,0 +1,54 @@
+﻿using DataAccess.Data;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StreletzNoticeBoard.Areas.Admin.Components
+{
+    /// <summary>
+    /// Модель представления для управления пользователями через админпанель
+    /// </summary>
+    public class UserViewModel
+    {
+        /// <summary>
+        /// Id пользователя.
+        /// </summary>
+        public string Id { get; set; }
+        /// <summary>
+        /// Логин.
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// Пароль (используется только при создании и редактировании).
+        /// </summary>
+        public string Password { get; set; }
+        /// <summary>
+        /// Роль.
+        /// </summary>
+        public string Role { get; set; }
+        /// <summary>
+        /// Роли для списков в UI.
+        /// </summary>
+        public IEnumerable<IdentityRole> Roles { get; set; }
+        /// <summary>
+        /// Конвертация IdentityUser в модель представления.
+        /// </summary>
+        /// <param name="rawUser">Объект IdentityUser.</param>
+        /// <param name="context">Контекст БД.</param>
+        /// <param name="userManager">Системный менеджер пользователей.</param>
+        /// <returns></returns>
+        public static UserViewModel Parse(IdentityUser rawUser, ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            UserViewModel result = new UserViewModel
+            {
+                Id = rawUser.Id,
+                UserName = rawUser.UserName,
+                Role = userManager.GetRolesAsync(rawUser).Result.First(),
+                Roles = context.Roles
+            };
+            return result;
+        }
+    }
+}
---
+++ StreletzNoticeBoard/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccess.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StreletzNoticeBoard.Areas.Admin.Components;
+
+namespace StreletzNoticeBoard.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        UserManager<IdentityUser> _userManager;
+       
+        public UserController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        // GET: User
+        public ActionResult Index()
+        {
+            IEnumerable<IdentityUser> rawUsers = _context.Users.OrderBy(x => x.UserName);
+            //IEnumerable<UserViewModel> users = rawUsers.Se;
+            IEnumerable<UserViewModel> users = new LinkedList<UserViewModel>();
+            foreach (IdentityUser rawuser in rawUsers)
+            {
+                UserViewModel user = UserViewModel.Parse(rawuser, _context, _userManager);
+                ((LinkedList<UserViewModel>)users).AddLast(user);
+            }
+            int a = rawUsers.Count();
+            return View(users);
+        }
+
+        // GET: User/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: User/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: User/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: User/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: User/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: User/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
