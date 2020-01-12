def8995aa68a06db1038cb877354a67f599efdfb
Streletz <streletzcoder@yandex.ru>
2020-01-13 01:41:49 +0300

    Объявления и общее
     - Карточка объявления для сайта;
     - Мелкие правки;
     - Правка зависимостей.

---
+++ StreletzNoticeBoard/Controllers/NoticesController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DataAccess.Data;
+using DataAccess.Data.Models;
+
+namespace StreletzNoticeBoard.Controllers
+{
+    public class NoticesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NoticesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Notices
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Notices.ToListAsync());
+        }
+
+        // GET: Notices/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var notice = await _context.Notices.Include(x => x.Category).Include(x=>x.Creator)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (notice == null)
+            {
+                return NotFound();
+            }
+
+            return View(notice);
+        }
+
+        // GET: Notices/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Notices/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Subject,Description,CreatedAt,IsActive")] Notice notice)
+        {
+            if (ModelState.IsValid)
+            {
+                notice.Id = Guid.NewGuid();
+                _context.Add(notice);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(notice);
+        }
+
+        // GET: Notices/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var notice = await _context.Notices.FindAsync(id);
+            if (notice == null)
+            {
+                return NotFound();
+            }
+            return View(notice);
+        }
+
+        // POST: Notices/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Subject,Description,CreatedAt,IsActive")] Notice notice)
+        {
+            if (id != notice.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(notice);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NoticeExists(notice.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(notice);
+        }
+
+        // GET: Notices/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var notice = await _context.Notices
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (notice == null)
+            {
+                return NotFound();
+            }
+
+            return View(notice);
+        }
+
+        // POST: Notices/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var notice = await _context.Notices.FindAsync(id);
+            _context.Notices.Remove(notice);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NoticeExists(Guid id)
+        {
+            return _context.Notices.Any(e => e.Id == id);
+        }
+    }
+}
